1c819ab99552321e6679023ed55c3af7925f1996
Patrick Goldinger <patrick.goldinger@pm.me>
2020-06-01 02:10:58 +0200

    Add KeyboardView preview in Looknfeel section

--- app/src/main/java/dev/patrickgold/florisboard/settings/LooknfeelFragment.kt
+++ app/src/main/java/dev/patrickgold/florisboard/settings/LooknfeelFragment.kt
@@ -1,16 +1,20 @@
 package dev.patrickgold.florisboard.settings
 
 import android.os.Bundle
+import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
-import android.widget.ListView
 import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceFragmentCompat
 import dev.patrickgold.florisboard.R
+import dev.patrickgold.florisboard.ime.text.keyboard.KeyboardMode
+import dev.patrickgold.florisboard.ime.text.keyboard.KeyboardView
+import dev.patrickgold.florisboard.ime.text.layout.LayoutManager
+import kotlinx.coroutines.*
 
-class LooknfeelFragment : Fragment() {
+class LooknfeelFragment : Fragment(), CoroutineScope by MainScope() {
     private lateinit var rootView: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,12 +28,31 @@ class LooknfeelFragment : Fragment() {
     ): View? {
         rootView = inflater.inflate(R.layout.settings_fragment_looknfeel, container, false) as LinearLayout
 
+        launch(Dispatchers.Default) {
+            val themeContext = ContextThemeWrapper(context, R.style.KeyboardTheme_MaterialLight)
+            val layoutManager = LayoutManager(themeContext)
+            layoutManager.autoFetchAssociationsFromPrefs()
+            val keyboardView = KeyboardView(themeContext)
+            keyboardView.isPreviewMode = true
+            keyboardView.setKeyboardMode(KeyboardMode.CHARACTERS, layoutManager)
+            keyboardView.updateVariation()
+            val cardLinearLayout = rootView.findViewById<LinearLayout>(R.id.settings__looknfeel__card_linear_layout)
+            withContext(Dispatchers.Main) {
+                cardLinearLayout.addView(keyboardView, 0)
+            }
+        }
+
         val transaction = childFragmentManager.beginTransaction()
         transaction.replace(R.id.settings__looknfeel__frame_container, PrefFragment())
         //transaction.addToBackStack(null)
         transaction.commit()
 
         return rootView
+    }
+
+    override fun onDestroy() {
+        cancel()
+        super.onDestroy()
     }
 
     class PrefFragment : PreferenceFragmentCompat() {
