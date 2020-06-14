884604395413f492d7b9847907138efc84fd69c2
Patrick Goldinger <patrick.goldinger@pm.me>
2020-06-15 00:49:22 +0200

    Implement preference caching for PrefHelper

--- app/src/main/java/dev/patrickgold/florisboard/ime/core/PrefHelper.kt
+++ app/src/main/java/dev/patrickgold/florisboard/ime/core/PrefHelper.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020 Patrick Goldinger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package dev.patrickgold.florisboard.ime.core
 
 import android.content.Context
@@ -6,43 +22,92 @@ import android.provider.Settings
 import androidx.preference.PreferenceManager
 import dev.patrickgold.florisboard.R
 
+/**
+ * Helper class for an organized access to the shared preferences.
+ */
 class PrefHelper(
     private val context: Context,
     val shared: SharedPreferences
 ) {
+    private val cacheBoolean: HashMap<String, Boolean> = hashMapOf()
+    private val cacheInt: HashMap<String, Int> = hashMapOf()
+    private val cacheString: HashMap<String, String> = hashMapOf()
+
     val advanced = Advanced(this)
     val looknfeel = Looknfeel(this)
     val theme = Theme(this)
 
+    /**
+     * Checks the cache if an entry for [key] exists, else calls [getPrefInternal] to retrieve the
+     * value. The type is automatically derived from the given [default] value.
+     * @return The value for [key] or [default].
+     */
     private inline fun <reified T> getPref(key: String, default: T): T {
         return when {
             false is T -> {
-                shared.getBoolean(key, default as Boolean) as T
+                (cacheBoolean[key] ?: getPrefInternal(key, default)) as T
             }
             0 is T -> {
-                shared.getInt(key, default as Int) as T
+                (cacheInt[key] ?: getPrefInternal(key, default)) as T
             }
             "" is T -> {
-                (shared.getString(key, default as String) ?: (default as String)) as T
+                (cacheString[key] ?: getPrefInternal(key, default)) as T
             }
             else -> null as T
         }
     }
 
+    /**
+     * Fetches the value for [key] from the shared preferences, puts the value into the
+     * corresponding cache and returns it.
+     * @return The value for [key] or [default].
+     */
+    private inline fun <reified T> getPrefInternal(key: String, default: T): T {
+        return when {
+            false is T -> {
+                val value = shared.getBoolean(key, default as Boolean)
+                cacheBoolean[key] = value
+                value as T
+            }
+            0 is T -> {
+                val value = shared.getInt(key, default as Int)
+                cacheInt[key] = value
+                value as T
+            }
+            "" is T -> {
+                val value = (shared.getString(key, default as String) ?: (default as String))
+                cacheString[key] = value
+                value as T
+            }
+            else -> null as T
+        }
+    }
+
+    /**
+     * Sets the [value] for [key] in the shared preferences, puts the value into the corresponding
+     * cache and returns it.
+     */
     private inline fun <reified T> setPref(key: String, value: T) {
         when {
             false is T -> {
                 shared.edit().putBoolean(key, value as Boolean).apply()
+                cacheBoolean[key] = value as Boolean
             }
             0 is T -> {
                 shared.edit().putInt(key, value as Int).apply()
+                cacheInt[key] = value as Int
             }
             "" is T -> {
                 shared.edit().putString(key, value as String).apply()
+                cacheString[key] = value as String
             }
         }
     }
 
+    /**
+     * Tells the [PreferenceManager] to set the defined preferences to their default values, if
+     * they have not been initialized yet.
+     */
     fun initDefaultPreferences() {
         PreferenceManager.setDefaultValues(context, R.xml.prefs_advanced, true)
         PreferenceManager.setDefaultValues(context, R.xml.prefs_keyboard, true)
@@ -50,18 +115,26 @@ class PrefHelper(
         PreferenceManager.setDefaultValues(context, R.xml.prefs_theme, true)
     }
 
+    /**
+     * Syncs the system preference values and clears the cache.
+     */
     fun sync() {
         val contentResolver = context.contentResolver
-
         looknfeel.soundEnabledSystem = Settings.System.getInt(
             contentResolver, Settings.System.SOUND_EFFECTS_ENABLED, 0
         ) != 0
-
         looknfeel.vibrationEnabledSystem = Settings.System.getInt(
             contentResolver, Settings.System.HAPTIC_FEEDBACK_ENABLED, 0
         ) != 0
+
+        cacheBoolean.clear()
+        cacheInt.clear()
+        cacheString.clear()
     }
 
+    /**
+     * Wrapper class for advanced preferences.
+     */
     class Advanced(private val prefHelper: PrefHelper) {
         companion object {
             const val SETTINGS_THEME =          "advanced__settings_theme"
@@ -76,6 +149,9 @@ class PrefHelper(
             private set
     }
 
+    /**
+     * Wrapper class for looknfeel preferences.
+     */
     class Looknfeel(private val prefHelper: PrefHelper) {
         companion object {
             const val HEIGHT_FACTOR =           "looknfeel__height_factor"
@@ -112,6 +188,9 @@ class PrefHelper(
             private set
     }
 
+    /**
+     * Wrapper class for theme preferences.
+     */
     class Theme(private val prefHelper: PrefHelper) {
         companion object {
             const val NAME =                    "theme__name"
