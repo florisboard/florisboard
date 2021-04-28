ff109714f94d73e3b38fd43a196d65e113ea7ae9
Patrick Goldinger <patrick.goldinger@pm.me>
2021-04-28 02:23:39 +0200

    Document TextKeyboardIconSet and TextKeyboardCache

--- app/src/main/java/dev/patrickgold/florisboard/ime/text/keyboard/TextKeyboardCache.kt
+++ app/src/main/java/dev/patrickgold/florisboard/ime/text/keyboard/TextKeyboardCache.kt
@@ -23,15 +23,22 @@ import androidx.collection.set
 import dev.patrickgold.florisboard.debug.*
 import dev.patrickgold.florisboard.ime.core.Subtype
 import kotlinx.coroutines.*
-import kotlinx.coroutines.sync.Mutex
 import java.util.*
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
+/**
+ * Helper class for caching computed text keyboards. Done to reduce the CPU strain and to reuse already allocated
+ * objects. The cache stores the deferred results of the keyboard, meaning that it can contain keyboards which haven't
+ * even finished computing. When getting a keyboard, the caller must always await the result thus. Additionally several
+ * clear methods are provided to clear the cache either partially or completely.
+ *
+ * @param ioDispatcher The dispatcher for the IO channel. Defaults to [Dispatchers.IO].
+ */
+@Suppress("MemberVisibilityCanBePrivate")
 class TextKeyboardCache(ioDispatcher: CoroutineDispatcher = Dispatchers.IO) {
     private val cache: EnumMap<KeyboardMode, SparseArrayCompat<Deferred<TextKeyboard>>> = EnumMap(KeyboardMode::class.java)
-    private val cacheGuard: Mutex = Mutex(locked = false)
     private val scope: CoroutineScope = CoroutineScope(ioDispatcher + SupervisorJob())
 
     init {
@@ -42,6 +49,9 @@ class TextKeyboardCache(ioDispatcher: CoroutineDispatcher = Dispatchers.IO) {
         }
     }
 
+    /**
+     * Clears all computed keyboards for all modes and all subtypes from this cache.
+     */
     fun clear() {
         flogDebug(LogTopic.TEXT_KEYBOARD_VIEW) { "Clear whole cache" }
         for (mode in KeyboardMode.values()) {
@@ -49,11 +59,21 @@ class TextKeyboardCache(ioDispatcher: CoroutineDispatcher = Dispatchers.IO) {
         }
     }
 
+    /**
+     * Clears all computed keyboards associated with given [mode].
+     *
+     * @param mode The keyboard mode for which the computed keyboards should be cleared from the cache.
+     */
     fun clear(mode: KeyboardMode) {
         flogDebug(LogTopic.TEXT_KEYBOARD_VIEW) { "Clear cache for mode '$mode'" }
         cache[mode]!!.clear()
     }
 
+    /**
+     * Clears all computed keyboards associated with given [subtype].
+     *
+     * @param subtype The subtype for which the computed keyboards should be cleared from the cache.
+     */
     fun clear(subtype: Subtype) {
         flogDebug(LogTopic.TEXT_KEYBOARD_VIEW) { "Clear cache for subtype '${subtype.toShortString()}'" }
         for (mode in KeyboardMode.values()) {
@@ -61,17 +81,43 @@ class TextKeyboardCache(ioDispatcher: CoroutineDispatcher = Dispatchers.IO) {
         }
     }
 
+    /**
+     * Clears all computed keyboards associated with given [mode] _and_ [subtype].
+     *
+     * @param mode The keyboard mode for which the computed keyboards should be cleared from the cache.*
+     * @param subtype The subtype for which the computed keyboards should be cleared from the cache.
+     */
     fun clear(mode: KeyboardMode, subtype: Subtype) {
         flogDebug(LogTopic.TEXT_KEYBOARD_VIEW) { "Clear cache for mode '$mode' and subtype '${subtype.toShortString()}'" }
         cache[mode]!!.remove(subtype.hashCode())
     }
 
+    /**
+     * Performs a get operation on the cache to retrieve the computed keyboard for given [mode] _and_ [subtype].
+     *
+     * @param mode The mode of the computed keyboard to get.
+     * @param subtype The subtype of the computed keyboard to get.
+     *
+     * @return The deferred computed keyboard or null if the cache does not have an entry associated with the given
+     *  params.
+     */
     fun getAsync(mode: KeyboardMode, subtype: Subtype): Deferred<TextKeyboard>? {
         return cache[mode]!![subtype.hashCode()].also {
             flogDebug(LogTopic.TEXT_KEYBOARD_VIEW) { "Get keyboard '$mode ${subtype.toShortString()}'" }
         }
     }
 
+    /**
+     * Performs a get operation on the cache to retrieve the computed keyboard for given [mode] _and_ [subtype]. If no
+     * entry for the given params can be found, [block] will be called and the deferred result of this block will
+     * instantly be put into the cache and returned.
+     *
+     * @param mode The mode of the computed keyboard to get.
+     * @param subtype The subtype of the computed keyboard to get.
+     * @param block The lambda expression which is invoked to provide a fallback computed keyboard.
+     *
+     * @return The deferred computed keyboard either from the cache or from [block].
+     */
     fun getOrElseAsync(mode: KeyboardMode, subtype: Subtype, block: suspend () -> TextKeyboard): Deferred<TextKeyboard> {
         contract {
             callsInPlace(block, InvocationKind.AT_MOST_ONCE)
@@ -86,6 +132,13 @@ class TextKeyboardCache(ioDispatcher: CoroutineDispatcher = Dispatchers.IO) {
         }
     }
 
+    /**
+     * Performs a set operation on the cache and assigns [keyboard] to the given params [mode] and [subtype].
+     *
+     * @param mode The mode of the computed keyboard to set.
+     * @param subtype The subtype of the computed keyboard to set.
+     * @param keyboard The deferred computed keyboard to set for the given params.
+     */
     fun set(mode: KeyboardMode, subtype: Subtype, keyboard: Deferred<TextKeyboard>) {
         flogDebug(LogTopic.TEXT_KEYBOARD_VIEW) { "Set keyboard '$mode ${subtype.toShortString()}'" }
         cache[mode]!![subtype.hashCode()] = keyboard
--- app/src/main/java/dev/patrickgold/florisboard/ime/text/keyboard/TextKeyboardIconSet.kt
+++ app/src/main/java/dev/patrickgold/florisboard/ime/text/keyboard/TextKeyboardIconSet.kt
@@ -27,9 +27,28 @@ import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
+/**
+ * Abstract class definition for providing icons to a keyboard view. This class has been introduced to remove the need
+ * for keyboard vies to re-fetch drawable resources every time they draw on the canvas. The exact implementation is
+ * dependent on the subclass.
+ */
 abstract class KeyboardIconSet {
+    /**
+     * Get the drawable for the given [id].
+     *
+     * @param id The Android resource id of the drawable which should be returned.
+     *
+     * @return The drawable for given [id] or null if this icon set does not contain a drawable for this id.
+     */
     abstract fun getDrawable(@DrawableRes id: Int): Drawable?
 
+    /**
+     * Performs [block] on the drawable with the given [id]. If no drawable for the id exists,[block] will not be
+     * called at all.
+     *
+     * @param id The Android resource id of the drawable which should be used to execute block with.
+     * @param block The block which should be executed with the returned drawable.
+     */
     inline fun withDrawable(@DrawableRes id: Int, block: Drawable.() -> Unit) {
         contract {
             callsInPlace(block, InvocationKind.AT_MOST_ONCE)
@@ -43,6 +62,9 @@ abstract class KeyboardIconSet {
     }
 }
 
+/**
+ * An icon set for [TextKeyboardView].
+ */
 class TextKeyboardIconSet private constructor(context: Context) : KeyboardIconSet() {
     private val drawableCache: Array<Drawable?> = Array(RES_ICON_IDS.size) {
         ContextCompat.getDrawable(context.applicationContext, RES_ICON_IDS[it])
@@ -71,6 +93,11 @@ class TextKeyboardIconSet private constructor(context: Context) : KeyboardIconSe
             R.drawable.ic_space_bar,
         )
 
+        /**
+         * Constructs a new icon set with given [context].
+         *
+         * @param context The context which should be used to retrieve the pre-defined drawable resource ids.
+         */
         fun new(context: Context): TextKeyboardIconSet {
             return TextKeyboardIconSet(context)
         }
