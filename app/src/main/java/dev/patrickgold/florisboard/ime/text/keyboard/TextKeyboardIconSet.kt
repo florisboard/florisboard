28f0657bd729ac955e5584609754efe09a843d03
Patrick Goldinger <patrick.goldinger@pm.me>
2021-05-04 17:45:28 +0200

    Improve and fix KeyboardIconSet (#778)

--- app/src/main/java/dev/patrickgold/florisboard/ime/text/keyboard/TextKeyboardIconSet.kt
+++ app/src/main/java/dev/patrickgold/florisboard/ime/text/keyboard/TextKeyboardIconSet.kt
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-@file:OptIn(ExperimentalContracts::class)
-
 package dev.patrickgold.florisboard.ime.text.keyboard
 
 import android.content.Context
@@ -23,51 +21,14 @@ import android.graphics.drawable.Drawable
 import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
 import dev.patrickgold.florisboard.R
-import kotlin.contracts.ExperimentalContracts
-import kotlin.contracts.InvocationKind
-import kotlin.contracts.contract
-
-/**
- * Abstract class definition for providing icons to a keyboard view. This class has been introduced to remove the need
- * for keyboard vies to re-fetch drawable resources every time they draw on the canvas. The exact implementation is
- * dependent on the subclass.
- */
-abstract class KeyboardIconSet {
-    /**
-     * Get the drawable for the given [id].
-     *
-     * @param id The Android resource id of the drawable which should be returned.
-     *
-     * @return The drawable for given [id] or null if this icon set does not contain a drawable for this id.
-     */
-    abstract fun getDrawable(@DrawableRes id: Int): Drawable?
-
-    /**
-     * Performs [block] on the drawable with the given [id]. If no drawable for the id exists,[block] will not be
-     * called at all.
-     *
-     * @param id The Android resource id of the drawable which should be used to execute block with.
-     * @param block The block which should be executed with the returned drawable.
-     */
-    inline fun withDrawable(@DrawableRes id: Int, block: Drawable.() -> Unit) {
-        contract {
-            callsInPlace(block, InvocationKind.AT_MOST_ONCE)
-        }
-        val drawable = getDrawable(id)
-        if (drawable != null) {
-            synchronized(drawable) {
-                block(drawable)
-            }
-        }
-    }
-}
+import dev.patrickgold.florisboard.ime.keyboard.KeyboardIconSet
 
 /**
  * An icon set for [TextKeyboardView].
  */
 class TextKeyboardIconSet private constructor(context: Context) : KeyboardIconSet() {
     private val drawableCache: Array<Drawable?> = Array(RES_ICON_IDS.size) {
-        ContextCompat.getDrawable(context.applicationContext, RES_ICON_IDS[it])
+        ContextCompat.getDrawable(context.applicationContext, RES_ICON_IDS[it])?.mutate()
     }
 
     companion object {
