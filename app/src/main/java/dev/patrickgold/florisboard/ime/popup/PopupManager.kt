a3fdad02e231a1b6c0f5f8de3dd0cd2c0ba727b6
waelwindows <waelwindows9922@gmail.com>
2021-08-10 22:18:50 +0300

    feat: Add char width switcher to CJK mod layouts
    Icons are yet to be added, so the labels will be strings for now

--- app/src/main/java/dev/patrickgold/florisboard/ime/popup/PopupManager.kt
+++ app/src/main/java/dev/patrickgold/florisboard/ime/popup/PopupManager.kt
@@ -49,7 +49,8 @@ class PopupManager<V : View>(
         KeyCode.SWITCH_TO_TEXT_CONTEXT,
         KeyCode.SWITCH_TO_MEDIA_CONTEXT,
         KeyCode.SWITCH_TO_CLIPBOARD_CONTEXT,
-        KeyCode.KANA_SWITCHER
+        KeyCode.KANA_SWITCHER,
+        KeyCode.CHAR_WIDTH_SWITCHER
     )
     private var keyPopupWidth: Int
     private var keyPopupHeight: Int
