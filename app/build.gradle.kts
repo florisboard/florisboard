e768af02e29f5cc6a3f9a1b08d282d698ed37ed0
Patrick Goldinger <patrick@patrickgold.dev>
2021-09-28 20:58:53 +0200

    Upgrade KotlinX Coroutine Library to 1.5.2 (#1295)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -145,7 +145,7 @@ dependencies {
     implementation("com.nambimobile.widgets:expandable-fab:1.0.2") // possibly remove after settings rework
     implementation("dev.patrickgold.jetpref:jetpref-datastore-model:0.1.0-alpha09")
     implementation("dev.patrickgold.jetpref:jetpref-ui-compose:0.1.0-alpha09")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
     implementation("androidx.room:room-runtime:2.3.0")
     kapt("androidx.room:room-compiler:2.3.0")
