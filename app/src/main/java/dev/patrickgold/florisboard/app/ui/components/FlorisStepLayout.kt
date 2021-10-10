04dacbc06002690bf6e53d225e3f1829c26438db
Patrick Goldinger <patrick@patrickgold.dev>
2021-10-11 01:29:49 +0200

    Add FlorisStepLayout and adapt setup screen to use it

---
+++ app/src/main/java/dev/patrickgold/florisboard/app/ui/components/FlorisStepLayout.kt
@@ -0,0 +1,243 @@
+/*
+ * Copyright (C) 2021 Patrick Goldinger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.patrickgold.florisboard.app.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+data class FlorisStep(
+    val id: Int,
+    val title: String,
+    val content: @Composable FlorisStepLayoutScope.() -> Unit,
+)
+
+class FlorisStepLayoutScope(columnScope: ColumnScope) : ColumnScope by columnScope {
+    @Composable
+    fun StepText(
+        text: String,
+        modifier: Modifier = Modifier,
+    ) {
+        Text(
+            modifier = modifier,
+            text = text,
+            textAlign = TextAlign.Justify,
+        )
+    }
+
+    @Composable
+    fun StepButton(
+        label: String,
+        modifier: Modifier = Modifier,
+        onClick: () -> Unit,
+    ) {
+        Button(
+            modifier = modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 16.dp),
+            onClick = onClick,
+        ) {
+            Text(text = label)
+        }
+    }
+}
+
+@Suppress("unused")
+class FlorisStepState private constructor(
+    private val currentAuto: MutableState<Int>,
+    private val currentManual: MutableState<Int> = mutableStateOf(-1),
+) {
+    companion object {
+        fun new(init: Int) = FlorisStepState(mutableStateOf(init))
+
+        val Saver = Saver<FlorisStepState, ArrayList<Int>>(
+            save = {
+                arrayListOf(it.currentAuto.value, it.currentManual.value)
+            },
+            restore = {
+                FlorisStepState(mutableStateOf(it[0]), mutableStateOf(it[1]))
+            },
+        )
+    }
+
+    fun getCurrent(): State<Int> {
+        return if (currentManual.value >= 0 && currentAuto.value >= currentManual.value) {
+            currentManual
+        } else {
+            currentAuto
+        }
+    }
+
+    fun getCurrentAuto(): State<Int> = currentAuto
+
+    fun getCurrentManual(): State<Int> = currentManual
+
+    fun setCurrentAuto(value: Int) {
+        currentAuto.value = value
+    }
+
+    fun setCurrentManual(value: Int) {
+        if (currentAuto.value == value) {
+            currentManual.value = -1
+        } else {
+            currentManual.value = value
+        }
+    }
+}
+
+@Composable
+fun FlorisStepLayout(
+    stepState: FlorisStepState,
+    steps: List<FlorisStep>,
+    modifier: Modifier = Modifier,
+    header: @Composable FlorisStepLayoutScope.() -> Unit = { },
+    footer: @Composable FlorisStepLayoutScope.() -> Unit = { },
+) {
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        val scope = FlorisStepLayoutScope(this)
+        header(scope)
+        for (step in steps) {
+            key(step.id) {
+                Step(
+                    ownStepId = step.id,
+                    stepState = stepState,
+                    title = step.title,
+                ) {
+                    step.content(FlorisStepLayoutScope(this))
+                }
+            }
+        }
+        footer(scope)
+    }
+}
+
+@Composable
+private fun ColumnScope.Step(
+    ownStepId: Int,
+    stepState: FlorisStepState,
+    title: String,
+    content: @Composable ColumnScope.() -> Unit,
+) {
+    val currentStepId by stepState.getCurrent()
+    val autoStepId by stepState.getCurrentAuto()
+    val backgroundColor = when (ownStepId) {
+        currentStepId -> MaterialTheme.colors.primary
+        else -> MaterialTheme.colors.onSurface.copy(alpha = 0.12f)
+    }
+    if (ownStepId == currentStepId) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.0f),
+        ) {
+            StepHeader(
+                backgroundColor = backgroundColor,
+                step = ownStepId,
+                title = title,
+            )
+            Box(modifier = Modifier.padding(start = 56.dp)) {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    content()
+                }
+            }
+        }
+    } else {
+        val modifier = when {
+            ownStepId <= autoStepId -> Modifier.clickable { stepState.setCurrentManual(ownStepId) }
+            else -> Modifier.alpha(ContentAlpha.disabled)
+        }
+        StepHeader(
+            modifier = modifier,
+            backgroundColor = backgroundColor,
+            step = ownStepId,
+            title = title,
+        )
+    }
+}
+
+@Composable
+private fun StepHeader(
+    modifier: Modifier = Modifier,
+    backgroundColor: Color,
+    contentColor: Color = contentColorFor(backgroundColor),
+    step: Int,
+    title: String,
+) {
+    Row(
+        modifier = modifier
+            .padding(vertical = 16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(end = 16.dp)
+                .size(40.dp)
+                .clip(CircleShape)
+                .background(backgroundColor),
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                text = step.toString(),
+                color = contentColor,
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .height(32.dp)
+                .weight(1.0f)
+                .clip(CircleShape)
+                .background(backgroundColor),
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                text = title,
+                color = contentColor,
+            )
+        }
+    }
+}
--- app/src/main/java/dev/patrickgold/florisboard/app/ui/setup/SetupScreen.kt
+++ app/src/main/java/dev/patrickgold/florisboard/app/ui/setup/SetupScreen.kt
@@ -19,8 +19,6 @@ package dev.patrickgold.florisboard.app.ui.setup
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -29,23 +27,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
-import androidx.compose.material.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import dev.patrickgold.florisboard.R
 import dev.patrickgold.florisboard.app.LocalIsFlorisBoardEnabled
@@ -54,6 +45,9 @@ import dev.patrickgold.florisboard.app.LocalNavController
 import dev.patrickgold.florisboard.app.res.stringRes
 import dev.patrickgold.florisboard.app.ui.Routes
 import dev.patrickgold.florisboard.app.ui.components.FlorisScreen
+import dev.patrickgold.florisboard.app.ui.components.FlorisStep
+import dev.patrickgold.florisboard.app.ui.components.FlorisStepLayout
+import dev.patrickgold.florisboard.app.ui.components.FlorisStepState
 import dev.patrickgold.florisboard.common.InputMethodUtils
 import dev.patrickgold.florisboard.common.launchUrl
 
@@ -74,182 +68,89 @@ fun SetupScreen() = FlorisScreen(
 
     val isFlorisBoardEnabled = LocalIsFlorisBoardEnabled.current
     val isFlorisBoardSelected = LocalIsFlorisBoardSelected.current
-    var currentStep by rememberSaveable {
+    val stepState = rememberSaveable(saver = FlorisStepState.Saver) {
         val initStep = when {
             !isFlorisBoardEnabled -> Step.EnableIme
             !isFlorisBoardSelected -> Step.SelectIme
             else -> Step.FinishUp
         }
-        mutableStateOf(initStep)
+        FlorisStepState.new(initStep)
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 16.dp),
-    ) {
-        SetupText(stringRes(R.string.setup__intro_message))
-        Spacer(modifier = Modifier.height(16.dp))
-        Step(
-            ownStep = Step.EnableIme,
-            currentStep = currentStep,
-            title = stringRes(R.string.setup__enable_ime__title)
-        ) {
-            SetupText(stringRes(R.string.setup__enable_ime__description))
-            SetupButton(label = stringRes(R.string.setup__enable_ime__open_settings_btn)) {
-                InputMethodUtils.showImeEnablerActivity(context)
-            }
-        }
-        Step(
-            ownStep = Step.SelectIme,
-            currentStep = currentStep,
-            title = stringRes(R.string.setup__select_ime__title)
-        ) {
-            SetupText(stringRes(R.string.setup__select_ime__description))
-            SetupButton(label = stringRes(R.string.setup__select_ime__switch_keyboard_btn)) {
-                InputMethodUtils.showImePicker(context)
-            }
-        }
-        Step(
-            ownStep = Step.FinishUp,
-            currentStep = currentStep,
-            title = stringRes(R.string.setup__finish_up__title)
-        ) {
-            SetupText(stringRes(R.string.setup__finish_up__description_p1))
-            SetupText(stringRes(R.string.setup__finish_up__description_p2))
-            SetupButton(label = stringRes(R.string.setup__finish_up__finish_btn)) {
-                this@FlorisScreen.prefs.internal.isImeSetUp.set(true)
-                navController.navigate(Routes.Settings.Home)
-            }
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center,
-        ) {
-            val privacyPolicyUrl = stringRes(R.string.florisboard__privacy_policy_url)
-            TextButton(onClick = { launchUrl(context, privacyPolicyUrl)}) {
-                Text(text = stringRes(R.string.setup__footer__privacy_policy))
-            }
-            Box(
-                modifier = Modifier
-                    .padding(horizontal = 8.dp)
-                    .size(12.dp, 4.dp)
-                    .clip(CircleShape)
-                    .background(MaterialTheme.colors.onSurface.copy(alpha = 0.12f))
-            )
-            val repositoryUrl = stringRes(R.string.florisboard__repo_url)
-            TextButton(onClick = { launchUrl(context, repositoryUrl) }) {
-                Text(text = stringRes(R.string.setup__footer__repository))
-            }
-        }
-        SideEffect {
-            currentStep = when {
+    SideEffect {
+        stepState.setCurrentAuto(
+            when {
                 !isFlorisBoardEnabled -> Step.EnableIme
                 !isFlorisBoardSelected -> Step.SelectIme
                 else -> Step.FinishUp
             }
-        }
-    }
-}
-
-@Composable
-fun SetupText(text: String) {
-    Text(
-        text = text,
-        textAlign = TextAlign.Justify,
-    )
-}
-
-@Composable
-fun ColumnScope.SetupButton(label: String, onClick: () -> Unit) {
-    Button(
-        modifier = Modifier
-            .align(Alignment.CenterHorizontally)
-            .padding(top = 16.dp),
-        onClick = onClick,
-    ) {
-        Text(text = label)
-    }
-}
-
-@Composable
-internal fun ColumnScope.Step(
-    ownStep: Int,
-    currentStep: Int,
-    title: String,
-    content: @Composable ColumnScope.() -> Unit,
-) {
-    val backgroundColor = when (ownStep) {
-        currentStep -> MaterialTheme.colors.primary
-        else -> MaterialTheme.colors.onSurface.copy(alpha = 0.12f)
-    }
-    if (ownStep == currentStep) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1.0f)
-        ) {
-            StepHeader(
-                backgroundColor = backgroundColor,
-                step = ownStep,
-                title = title,
-            )
-            Box(modifier = Modifier.padding(start = 56.dp)) {
-                Column(modifier = Modifier.fillMaxSize()) {
-                    content()
-                }
-            }
-        }
-    } else {
-        StepHeader(
-            backgroundColor = backgroundColor,
-            step = ownStep,
-            title = title,
         )
     }
-}
 
-@Composable
-fun StepHeader(
-    backgroundColor: Color,
-    contentColor: Color = contentColorFor(backgroundColor),
-    step: Int,
-    title: String,
-) {
-    Row(
+    FlorisStepLayout(
         modifier = Modifier
-            .padding(vertical = 16.dp),
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        Box(
-            modifier = Modifier
-                .padding(end = 16.dp)
-                .size(40.dp)
-                .clip(CircleShape)
-                .background(backgroundColor),
-        ) {
-            Text(
-                modifier = Modifier.align(Alignment.Center),
-                text = step.toString(),
-                color = contentColor,
-            )
-        }
-
-        Box(
-            modifier = Modifier
-                .height(32.dp)
-                .weight(1.0f)
-                .clip(CircleShape)
-                .background(backgroundColor),
-        ) {
-            Text(
-                modifier = Modifier.align(Alignment.Center),
-                text = title,
-                color = contentColor,
-            )
-        }
-    }
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
+        stepState = stepState,
+        header = {
+            StepText(stringRes(R.string.setup__intro_message))
+            Spacer(modifier = Modifier.height(16.dp))
+        },
+        steps = listOf(
+            FlorisStep(
+                id = Step.EnableIme,
+                title = stringRes(R.string.setup__enable_ime__title),
+            ) {
+                StepText(stringRes(R.string.setup__enable_ime__description))
+                StepButton(label = stringRes(R.string.setup__enable_ime__open_settings_btn)) {
+                    InputMethodUtils.showImeEnablerActivity(context)
+                }
+            },
+            FlorisStep(
+                id = Step.SelectIme,
+                title = stringRes(R.string.setup__select_ime__title),
+            ) {
+                StepText(stringRes(R.string.setup__select_ime__description))
+                StepButton(label = stringRes(R.string.setup__select_ime__switch_keyboard_btn)) {
+                    InputMethodUtils.showImePicker(context)
+                }
+            },
+            FlorisStep(
+                id = Step.FinishUp,
+                title = stringRes(R.string.setup__finish_up__title),
+            ) {
+                StepText(stringRes(R.string.setup__finish_up__description_p1))
+                StepText(stringRes(R.string.setup__finish_up__description_p2))
+                StepButton(label = stringRes(R.string.setup__finish_up__finish_btn)) {
+                    this@FlorisScreen.prefs.internal.isImeSetUp.set(true)
+                    navController.navigate(Routes.Settings.Home)
+                }
+            },
+        ),
+        footer = {
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+            ) {
+                val privacyPolicyUrl = stringRes(R.string.florisboard__privacy_policy_url)
+                TextButton(onClick = { launchUrl(context, privacyPolicyUrl)}) {
+                    Text(text = stringRes(R.string.setup__footer__privacy_policy))
+                }
+                Box(
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp)
+                        .size(12.dp, 4.dp)
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colors.onSurface.copy(alpha = 0.12f))
+                )
+                val repositoryUrl = stringRes(R.string.florisboard__repo_url)
+                TextButton(onClick = { launchUrl(context, repositoryUrl) }) {
+                    Text(text = stringRes(R.string.setup__footer__repository))
+                }
+            }
+        },
+    )
 }
