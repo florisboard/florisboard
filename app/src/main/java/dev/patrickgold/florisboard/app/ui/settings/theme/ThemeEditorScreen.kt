cd2a0000c0cb3a63f7d3a6eaf0fd974f51be170d
Patrick Goldinger <patrick@patrickgold.dev>
2022-01-21 00:50:18 +0100

    Add rule and properties view in stylesheet editor

--- app/src/main/java/dev/patrickgold/florisboard/app/ui/settings/theme/ThemeEditorScreen.kt
+++ app/src/main/java/dev/patrickgold/florisboard/app/ui/settings/theme/ThemeEditorScreen.kt
@@ -17,11 +17,14 @@
 package dev.patrickgold.florisboard.app.ui.settings.theme
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Card
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -33,12 +36,18 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import dev.patrickgold.florisboard.R
 import dev.patrickgold.florisboard.app.res.stringRes
 import dev.patrickgold.florisboard.app.ui.components.FlorisIconButton
+import dev.patrickgold.florisboard.app.ui.components.FlorisOutlinedBox
 import dev.patrickgold.florisboard.app.ui.components.FlorisScreen
+import dev.patrickgold.florisboard.app.ui.components.FlorisTextButton
+import dev.patrickgold.florisboard.app.ui.components.rippleClickable
 import dev.patrickgold.florisboard.ime.theme.FlorisImeUi
+import dev.patrickgold.florisboard.ime.theme.FlorisImeUiSpec
 import dev.patrickgold.florisboard.ime.theme.ThemeExtensionComponentEditor
 import dev.patrickgold.florisboard.res.cache.CacheManager
 import dev.patrickgold.florisboard.res.io.readJson
@@ -50,6 +59,7 @@ import dev.patrickgold.florisboard.snygg.SnyggRuleEditor
 import dev.patrickgold.florisboard.snygg.SnyggStylesheet
 import dev.patrickgold.florisboard.snygg.SnyggStylesheetEditor
 import dev.patrickgold.florisboard.snygg.SnyggStylesheetJsonConfig
+import dev.patrickgold.jetpref.material.ui.JetPrefListItem
 
 @Composable
 fun ThemeEditorScreen(
@@ -100,31 +110,145 @@ fun ThemeEditorScreen(
         )
     }
 
+    // TODO: lazy column??
     content {
         BackHandler {
             handleBackPress()
         }
 
+        if (stylesheetEditor.rules.isEmpty()) {
+            Text(
+                modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp),
+                text = stringRes(R.string.settings__theme_editor__no_rules_defined),
+                fontStyle = FontStyle.Italic,
+            )
+        }
         for ((rule, propertySet) in stylesheetEditor.rules) key(rule) {
-            Card(
+            val propertySetSpec = FlorisImeUiSpec.propertySetSpec(rule.element)
+            FlorisOutlinedBox(
                 modifier = Modifier
                     .padding(vertical = 8.dp, horizontal = 16.dp)
                     .fillMaxWidth(),
             ) {
-                SnyggRuleRow(
-                    rule = rule,
-                    level = snyggLevel,
-                    onEditRuleBtnClick = { /*TODO*/ },
-                    onAddPropertyBtnClick = { },
-                )
                 Column(modifier = Modifier.fillMaxWidth()) {
+                    SnyggRuleRow(
+                        rule = rule,
+                        level = snyggLevel,
+                        onEditRuleBtnClick = { /*TODO*/ },
+                        onAddPropertyBtnClick = { },
+                    )
                     for ((propertyName, propertyValue) in propertySet.properties) {
-                        Text(text = translatePropertyName(propertyName, snyggLevel))
-                        Text(text = propertyValue.toString())
+                        val propertySpec = propertySetSpec?.propertySpec(propertyName)
+                        if (propertySpec != null && propertySpec.level <= snyggLevel) {
+                            JetPrefListItem(
+                                modifier = Modifier.rippleClickable {  },
+                                text = translatePropertyName(propertyName, snyggLevel),
+                                secondaryText = propertyValue.toString(),
+                            )
+                        }
                     }
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 6.dp),
+                ) {
+                    FlorisTextButton(
+                        onClick = { workspace.update { stylesheetEditor.rules.remove(rule) } },
+                        icon = painterResource(R.drawable.ic_delete),
+                        text = stringRes(R.string.action__delete),
+                        colors = ButtonDefaults.textButtonColors(
+                            contentColor = MaterialTheme.colors.error,
+                        ),
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    FlorisTextButton(
+                        onClick = { },
+                        icon = painterResource(R.drawable.ic_edit),
+                        text = stringRes(R.string.action__edit),
+                    )
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun SnyggRuleRow(
+    rule: SnyggRuleEditor,
+    level: SnyggLevel,
+    onEditRuleBtnClick: () -> Unit,
+    onAddPropertyBtnClick: () -> Unit,
+) {
+    @Composable
+    fun Selector(text: String) {
+        Text(
+            modifier = Modifier
+                .background(MaterialTheme.colors.primaryVariant)
+                .padding(end = 4.dp),
+            text = text,
+            style = MaterialTheme.typography.body2,
+            fontFamily = FontFamily.Monospace,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
+    }
+
+    @Composable
+    fun AttributesList(text: String, list: String) {
+        Text(
+            text = "$text = $list",
+            style = MaterialTheme.typography.body2,
+            color = LocalContentColor.current.copy(alpha = 0.56f),
+            fontFamily = FontFamily.Monospace,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
+    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 6.dp),
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(vertical = 8.dp, horizontal = 10.dp),
+        ) {
+            Text(
+                text = rule.translateElementName(level),
+                style = MaterialTheme.typography.body2,
+                fontFamily = FontFamily.Monospace,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+            Row(modifier = Modifier.fillMaxWidth()) {
+                if (rule.pressedSelector) {
+                    Selector(text = "pressed")
+                }
+                if (rule.focusSelector) {
+                    Selector(text = "focus")
+                }
+                if (rule.disabledSelector) {
+                    Selector(text = "disabled")
+                }
+            }
+            if (rule.codes.isNotEmpty()) {
+                AttributesList(text = "codes", list = remember(rule.codes) { rule.codes.toString() })
+            }
+            if (rule.modes.isNotEmpty()) {
+                AttributesList(text = "modes", list = remember(rule.modes) { rule.modes.toString() })
+            }
+        }
+        FlorisTextButton(
+            onClick = { },
+            icon = painterResource(R.drawable.ic_add),
+            text = stringRes(R.string.action__add),
+            colors = ButtonDefaults.textButtonColors(
+                contentColor = MaterialTheme.colors.secondary,
+            ),
+        )
     }
 }
 
@@ -200,31 +324,5 @@ private fun translatePropertyName(propertyName: String, level: SnyggLevel): Stri
         } else {
             propertyName
         }
-    }
-}
-
-@Composable
-private fun SnyggRuleRow(
-    rule: SnyggRuleEditor,
-    level: SnyggLevel,
-    onEditRuleBtnClick: () -> Unit,
-    onAddPropertyBtnClick: () -> Unit,
-) {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        Column(modifier = Modifier.weight(1f)) {
-            Text(
-                text = rule.translateElementName(level),
-                style = MaterialTheme.typography.body2,
-                fontFamily = FontFamily.Monospace,
-            )
-        }
-        FlorisIconButton(
-            onClick = onEditRuleBtnClick,
-            icon = painterResource(R.drawable.ic_edit),
-        )
-        FlorisIconButton(
-            onClick = onEditRuleBtnClick,
-            icon = painterResource(R.drawable.ic_add),
-        )
     }
 }
