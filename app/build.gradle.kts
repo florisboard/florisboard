a8b0a6d555be3a84f8ffa327378535b7976d1959
Patrick Goldinger <patrick@patrickgold.dev>
2022-02-03 20:40:34 +0100

    Upgrade dependencies / Upgrade Kotlin to 1.6.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.1.0-rc01"
+        kotlinCompilerExtensionVersion = "1.1.0-rc03"
     }
 
     externalNativeBuild {
@@ -144,22 +144,22 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.4.0")
     implementation("androidx.autofill:autofill:1.1.0")
     implementation("androidx.collection:collection-ktx:1.2.0")
-    implementation("androidx.compose.material:material:1.1.0-rc01")
-    implementation("androidx.compose.runtime:runtime-livedata:1.1.0-rc01")
-    implementation("androidx.compose.ui:ui:1.1.0-rc01")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.1.0-rc01")
+    implementation("androidx.compose.material:material:1.1.0-rc03")
+    implementation("androidx.compose.runtime:runtime-livedata:1.1.0-rc03")
+    implementation("androidx.compose.ui:ui:1.1.0-rc03")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.1.0-rc03")
     implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.core:core-splashscreen:1.0.0-alpha02")
-    implementation("androidx.navigation:navigation-compose:2.4.0-rc01")
+    implementation("androidx.core:core-splashscreen:1.0.0-beta01")
+    implementation("androidx.navigation:navigation-compose:2.4.0")
     implementation("com.google.accompanist:accompanist-flowlayout:0.20.2")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.20.2")
-    implementation("dev.patrickgold.jetpref:jetpref-datastore-model:0.1.0-beta05")
-    implementation("dev.patrickgold.jetpref:jetpref-datastore-ui:0.1.0-beta05")
-    implementation("dev.patrickgold.jetpref:jetpref-material-ui:0.1.0-beta05")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
-    implementation("androidx.room:room-runtime:2.4.0")
-    kapt("androidx.room:room-compiler:2.4.0")
+    implementation("dev.patrickgold.jetpref:jetpref-datastore-model:0.1.0-beta06")
+    implementation("dev.patrickgold.jetpref:jetpref-datastore-ui:0.1.0-beta06")
+    implementation("dev.patrickgold.jetpref:jetpref-material-ui:0.1.0-beta06")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    implementation("androidx.room:room-runtime:2.4.1")
+    kapt("androidx.room:room-compiler:2.4.1")
 
     testImplementation("io.kotest:kotest-runner-junit5:5.1.0")
     testImplementation("io.kotest:kotest-assertions-core:5.1.0")
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.1.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.10")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.4")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.7.1.1")
     }
