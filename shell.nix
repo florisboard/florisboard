1a04a2f49d915ce9096a7b212d17c3a25f63c51e
waelwindows <waelwindows9922@gmail.com>
2022-03-25 17:17:40 +0300

    fix(build): Use correct aapt2 path in `shell.nix`

--- shell.nix
+++ shell.nix
@@ -2,6 +2,7 @@ with import <nixpkgs> {};
 
 let
   cmakeVersion = "3.18.1";
+  buildToolsVersion = "31.0.0" ;
 
   # Use cmakeVersion when you define androidComposition
   env = (pkgs.androidenv.override {
@@ -11,7 +12,7 @@ let
   androidComposition = env.composeAndroidPackages {
     platformVersions = [ "31" ];
     toolsVersion = "26.1.1";
-    buildToolsVersions = [ "31.0.0" ];
+    buildToolsVersions = [ buildToolsVersion ];
     ndkVersions = ["22.1.7171670"];
     cmakeVersions = [ cmakeVersion ];
     includeNDK = true;
@@ -20,6 +21,8 @@ in
 pkgs.mkShell rec {
   ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
   ANDROID_NDK_ROOT = "${ANDROID_SDK_ROOT}/ndk-bundle";
+
+  GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${buildToolsVersion }/aapt2";
 
   # Use the same cmakeVersion here
   shellHook = ''
