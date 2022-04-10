76efc0a0e0a8242618a36902006099f6715a921f
Patrick Goldinger <patrick@patrickgold.dev>
2022-04-10 14:53:48 +0200

    Upgrade AGP to 7.1.3 / Upgrade other dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.2")
+        classpath("com.android.tools.build:gradle:7.1.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.10")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.5")
-        classpath("de.mannodermaus.gradle.plugins:android-junit5:1.7.1.1")
+        classpath("de.mannodermaus.gradle.plugins:android-junit5:1.8.2.0")
     }
 }
 
