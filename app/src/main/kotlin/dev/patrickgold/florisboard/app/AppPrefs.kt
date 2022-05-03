aaae483ad8f93ee2a66b403f13674ee5813b365c
Patrick Goldinger <patrick@patrickgold.dev>
2022-05-03 19:37:23 +0200

    Update ROADMAP.md and home screen message

--- app/src/main/kotlin/dev/patrickgold/florisboard/app/AppPrefs.kt
+++ app/src/main/kotlin/dev/patrickgold/florisboard/app/AppPrefs.kt
@@ -326,7 +326,7 @@ class AppPrefs : PreferenceModel("florisboard-app-prefs") {
     val internal = Internal()
     inner class Internal {
         val homeIsBetaToolboxCollapsed = boolean(
-            key = "internal__home_is_beta_toolbox_collapsed_0314release",
+            key = "internal__home_is_beta_toolbox_collapsed_0316beta01",
             default = false,
         )
         val isImeSetUp = boolean(
