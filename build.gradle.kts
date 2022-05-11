d9cd36966e2aec589f43645f40a6c0b33152dc85
Patrick Goldinger <patrick@patrickgold.dev>
2022-05-11 22:59:37 +0200

    Upgrade Gradle to 7.3.3 and AGP to 7.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.3")
+        classpath("com.android.tools.build:gradle:7.2.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.10")
         classpath("com.google.devtools.ksp:symbol-processing-gradle-plugin:1.6.10-1.0.4")
