977b32de6e89cb9dafd67c6dde4272aefd010ace
Patrick Goldinger <patrick@patrickgold.dev>
2022-05-14 02:06:37 +0200

    Add punctuation rules to keyboard extension (#596, #1828)

--- app/src/main/kotlin/dev/patrickgold/florisboard/ime/editor/EditorInstance.kt
+++ app/src/main/kotlin/dev/patrickgold/florisboard/ime/editor/EditorInstance.kt
@@ -32,7 +32,6 @@ import dev.patrickgold.florisboard.ime.clipboard.provider.ClipboardFileStorage
 import dev.patrickgold.florisboard.ime.clipboard.provider.ClipboardItem
 import dev.patrickgold.florisboard.ime.clipboard.provider.ItemType
 import dev.patrickgold.florisboard.ime.keyboard.KeyboardMode
-import dev.patrickgold.florisboard.ime.nlp.TextProcessor
 import dev.patrickgold.florisboard.ime.input.InputShiftState
 import dev.patrickgold.florisboard.ime.text.composing.Appender
 import dev.patrickgold.florisboard.ime.text.composing.Composer
@@ -42,6 +41,7 @@ import dev.patrickgold.florisboard.lib.android.AndroidVersion
 import dev.patrickgold.florisboard.lib.android.showShortToast
 import dev.patrickgold.florisboard.lib.devtools.flogDebug
 import dev.patrickgold.florisboard.lib.ext.ExtensionComponentName
+import dev.patrickgold.florisboard.nlpManager
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -56,6 +56,7 @@ class EditorInstance(context: Context) : AbstractEditorInstance(context) {
     private val appContext by context.appContext()
     private val clipboardManager by context.clipboardManager()
     private val keyboardManager by context.keyboardManager()
+    private val nlpManager by context.nlpManager()
     private val scope = MainScope()
 
     private val activeState get() = keyboardManager.activeState
@@ -452,9 +453,11 @@ class EditorInstance(context: Context) : AbstractEditorInstance(context) {
     private fun PhantomSpaceState.determine(text: String, forceActive: Boolean = false): Boolean {
         val content = activeContent
         val selection = content.selection
-        val isWordComponent = TextProcessor.isWord(text)
-        return (isActive || forceActive) && selection.isValid && selection.start > 0 &&
-            content.getTextBeforeCursor(1) != SPACE && isWordComponent
+        if (!(isActive || forceActive) || selection.isNotValid || selection.start <= 0) return false
+        val textBefore = content.getTextBeforeCursor(2)
+        val punctuationRule = nlpManager.getActivePunctuationRule()
+        return punctuationRule.symbolsPrecedingSpace.matches(textBefore) &&
+            punctuationRule.symbolsFollowingSpace.matches(text)
     }
 
     class PhantomSpaceState {
