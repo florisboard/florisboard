a7bc56f262d9d3d5ff566489bdd5c737119b4c04
MHKalantarian <MHKalantarian@gmail.com>
2022-06-06 12:46:26 +0430

    Internal text field extracted

--- app/src/main/kotlin/dev/patrickgold/florisboard/lib/compose/FlorisTextFields.kt
+++ app/src/main/kotlin/dev/patrickgold/florisboard/lib/compose/FlorisTextFields.kt
@@ -16,6 +16,10 @@
 
 package dev.patrickgold.florisboard.lib.compose
 
+import android.graphics.Rect
+import android.text.InputType
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
@@ -46,11 +50,16 @@ import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.takeOrElse
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextDirection
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import dev.patrickgold.florisboard.FlorisImeService
 import dev.patrickgold.florisboard.app.apptheme.outline
 import dev.patrickgold.florisboard.lib.ValidationResult
 
@@ -189,6 +198,165 @@ fun FlorisOutlinedTextField(
                 }
             },
         )
+    }
+
+    if (showValidationHint && validationResult?.isValid() == true && validationResult.hasHintMessage()) {
+        Text(
+            text = validationResult.hintMessage(),
+            style = MaterialTheme.typography.body2,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.56f),
+        )
+    }
+
+    if (showValidationError && validationResult?.isInvalid() == true && validationResult.hasErrorMessage()) {
+        Text(
+            text = validationResult.errorMessage(),
+            style = MaterialTheme.typography.body2,
+            color = MaterialTheme.colors.error,
+        )
+    }
+}
+
+@Composable
+fun FlorisInternalTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    textStyle: TextStyle = TextStyle.Default,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = false,
+    maxLines: Int = Int.MAX_VALUE,
+    placeholder: String? = null,
+    isError: Boolean = false,
+    showValidationHint: Boolean = true,
+    showValidationError: Boolean = false,
+    validationResult: ValidationResult? = null,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    shape: Shape = MaterialTheme.shapes.small,
+    colors: TextFieldColors = TextFieldDefaults.outlinedTextFieldColors(
+        unfocusedBorderColor = MaterialTheme.colors.outline,
+        disabledBorderColor = MaterialTheme.colors.outline,
+    ),
+) {
+    val textColor = textStyle.color.takeOrElse {
+        colors.textColor(enabled).value
+    }
+    val imeOptions: Int = when (keyboardOptions.imeAction) {
+        ImeAction.Done -> EditorInfo.IME_ACTION_DONE
+        ImeAction.Go -> EditorInfo.IME_ACTION_GO
+        ImeAction.Next -> EditorInfo.IME_ACTION_NEXT
+        ImeAction.Previous -> EditorInfo.IME_ACTION_PREVIOUS
+        ImeAction.Search -> EditorInfo.IME_ACTION_SEARCH
+        ImeAction.Send -> EditorInfo.IME_ACTION_SEND
+        else -> EditorInfo.IME_ACTION_NONE
+    }
+    val inputType: Int = when (keyboardOptions.keyboardType) {
+        KeyboardType.Text -> InputType.TYPE_CLASS_TEXT
+        KeyboardType.Number -> InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_VARIATION_NORMAL
+        KeyboardType.Phone -> InputType.TYPE_CLASS_PHONE
+        KeyboardType.Uri -> InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_URI
+        KeyboardType.Email -> InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+        KeyboardType.Password -> InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        KeyboardType.NumberPassword -> InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_VARIATION_PASSWORD
+        else -> InputType.TYPE_NULL
+    }
+    val inputCapitalization: Int = when (keyboardOptions.capitalization) {
+        KeyboardCapitalization.Characters -> InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS
+        KeyboardCapitalization.Words -> InputType.TYPE_TEXT_FLAG_CAP_WORDS
+        KeyboardCapitalization.Sentences -> InputType.TYPE_TEXT_FLAG_CAP_SENTENCES
+        else -> 0
+    }
+    val mergedTextStyle = textStyle.copy(color = textColor, textDirection = TextDirection.Content)
+    val isFocused by interactionSource.collectIsFocusedAsState()
+    val isErrorState = isError || (showValidationError && validationResult?.isInvalid() == true)
+
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+        Surface(
+            modifier = modifier,
+            color = colors.backgroundColor(enabled).value,
+            border = if (isErrorState && enabled) {
+                BorderStroke(ButtonDefaults.OutlinedBorderSize, MaterialTheme.colors.error)
+            } else if (isFocused) {
+                BorderStroke(ButtonDefaults.OutlinedBorderSize, MaterialTheme.colors.primary)
+            } else {
+                ButtonDefaults.outlinedBorder
+            },
+            shape = shape,
+        ) {
+            Box(
+                modifier = Modifier
+                    .defaultMinSize(
+                        minWidth = ButtonDefaults.MinWidth,
+                        minHeight = 40.dp,
+                    )
+                    .padding(ButtonDefaults.ContentPadding),
+                contentAlignment = Alignment.CenterStart,
+            ) {
+                ProvideTextStyle(value = mergedTextStyle) {
+                    AndroidView(
+                        modifier = Modifier.fillMaxWidth(), // Occupy the max size in the Compose UI tree
+                        factory = { context ->
+                            object : EditText(context) {
+                                override fun onTextChanged(
+                                    text: CharSequence?,
+                                    start: Int,
+                                    lengthBefore: Int,
+                                    lengthAfter: Int
+                                ) {
+                                    super.onTextChanged(text, start, lengthBefore, lengthAfter)
+                                    if (text != null) {
+                                        onValueChange(text.toString())
+                                    }
+                                }
+
+                                override fun onFocusChanged(
+                                    focused: Boolean,
+                                    direction: Int,
+                                    previouslyFocusedRect: Rect?
+                                ) {
+                                    super.onFocusChanged(focused, direction, previouslyFocusedRect)
+                                    if (focused)
+                                        FlorisImeService.setCurrentInputConnection(onCreateInputConnection(EditorInfo()))
+                                    else
+                                        FlorisImeService.setCurrentInputConnection(null)
+                                }
+
+                                override fun onEditorAction(actionCode: Int) {
+                                    super.onEditorAction(actionCode)
+                                    when (actionCode) {
+                                        EditorInfo.IME_ACTION_DONE -> keyboardActions.onDone
+                                        EditorInfo.IME_ACTION_GO -> keyboardActions.onGo
+                                        EditorInfo.IME_ACTION_NEXT -> keyboardActions.onNext
+                                        EditorInfo.IME_ACTION_PREVIOUS -> keyboardActions.onPrevious
+                                        EditorInfo.IME_ACTION_SEARCH -> keyboardActions.onSearch
+                                        EditorInfo.IME_ACTION_SEND -> keyboardActions.onSend
+                                    }
+                                }
+                            }.apply {
+                                this.isEnabled = enabled
+                                this.isSingleLine = singleLine
+                                this.maxLines = maxLines
+                                this.setTextColor(textColor.value.toInt())
+
+                                this.imeOptions = imeOptions
+                                this.inputType =
+                                    inputType or if (keyboardOptions.autoCorrect) InputType.TYPE_TEXT_FLAG_AUTO_CORRECT else 0 or inputCapitalization
+                            }
+                        },
+                        update = { it.setText(value) }
+                    )
+                }
+                if (!placeholder.isNullOrBlank()) {
+                    Text(
+                        text = placeholder,
+                        style = MaterialTheme.typography.body2,
+                        color = MaterialTheme.colors.onSurface.copy(alpha = 0.56f),
+                    )
+                }
+            }
+        }
     }
 
     if (showValidationHint && validationResult?.isValid() == true && validationResult.hasHintMessage()) {
