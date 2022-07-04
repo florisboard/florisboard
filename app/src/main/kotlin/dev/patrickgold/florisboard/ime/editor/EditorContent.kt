3d92bd05841cfe14e18a7c9fa21ac78c5d46c47c
Patrick Goldinger <patrick@patrickgold.dev>
2022-07-04 23:10:03 +0200

    Document EditorContent getters and companion object

--- app/src/main/kotlin/dev/patrickgold/florisboard/ime/editor/EditorContent.kt
+++ app/src/main/kotlin/dev/patrickgold/florisboard/ime/editor/EditorContent.kt
@@ -35,34 +35,73 @@ data class EditorContent(
     val localComposing: EditorRange,
     val localCurrentWord: EditorRange,
 ) {
+    /**
+     * The text before the selection as a new string. This may be the whole text before the selection or only a subset,
+     * depending on the cache and app configuration. Can also be empty for raw editors or if there is no text before
+     * the selection.
+     */
     val textBeforeSelection: String
         get() = if (localSelection.isValid) text.safeSubstring(0, localSelection.start) else ""
 
+    /**
+     * The selected text as a new string. This is always either the entire selected text or an empty string.
+     */
     val selectedText: String
         get() = if (localSelection.isValid) text.safeSubstring(localSelection.start, localSelection.end) else ""
 
+    /**
+     * The text after the selection as a new string. This may be the whole text after the selection or only a subset,
+     * depending on the cache and app configuration. Can also be empty for raw editors or if there is no text after the
+     * selection.
+     */
     val textAfterSelection: String
         get() = if (localSelection.isValid) text.safeSubstring(localSelection.end) else ""
 
+    /**
+     * The selection reported by the editor, with [offset] included.
+     */
     val selection: EditorRange
         get() = if (offset > 0) localSelection.translatedBy(offset) else localSelection
 
+    /**
+     * The composing region for the editor, with [offset] included. May be intentionally invalid even if there is a
+     * current word if the user has requested to disable the composing region.
+     */
     val composing: EditorRange
         get() = if (offset > 0) localComposing.translatedBy(offset) else localComposing
 
+    /**
+     * The composing region text as a new string. May be intentionally empty even if there is a current word if the
+     * user has requested to disable the composing region. This is always either the entire composing text or an empty
+     * string.
+     */
     val composingText: String
         get() = if (localComposing.isValid) text.safeSubstring(localComposing.start, localComposing.end) else ""
 
+    /**
+     * The current word for the editor (typically the same as [localComposing]), with [offset] included.
+     */
     val currentWord: EditorRange
         get() = if (offset > 0) localCurrentWord.translatedBy(offset) else localCurrentWord
 
+    /**
+     * The current word for the editor (typically the same as [localComposing]) as a new string. This is always either
+     * the current word or an empty string.
+     */
     val currentWordText: String
         get() = if (localCurrentWord.isValid) text.safeSubstring(localCurrentWord.start, localCurrentWord.end) else ""
 
     companion object {
+        /**
+         * Default editor content which indicates an unspecified content. This is used for raw editors or if there is
+         * an error in the communication between the keyboard and the app.
+         */
         val Unspecified =
             EditorContent("", -1, EditorRange.Unspecified, EditorRange.Unspecified, EditorRange.Unspecified)
 
+        /**
+         * Allows to instantiate a selection-only content, primarily used for mass-selection event handling.
+         */
         fun selectionOnly(selection: EditorRange) =
             EditorContent("", -1, selection, EditorRange.Unspecified, EditorRange.Unspecified)
     }
