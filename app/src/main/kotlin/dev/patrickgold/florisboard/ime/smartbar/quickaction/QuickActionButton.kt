eb30eed735431cb4cbcda9b61e99d53633741179
Patrick Goldinger <patrick@patrickgold.dev>
2022-08-11 18:51:30 +0200

    Fix quick action icon size too small for landscape (#1781)

--- app/src/main/kotlin/dev/patrickgold/florisboard/ime/smartbar/quickaction/QuickActionButton.kt
+++ app/src/main/kotlin/dev/patrickgold/florisboard/ime/smartbar/quickaction/QuickActionButton.kt
@@ -32,6 +32,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -40,6 +41,7 @@ import dev.patrickgold.florisboard.ime.keyboard.computeIconResId
 import dev.patrickgold.florisboard.ime.keyboard.computeLabel
 import dev.patrickgold.florisboard.ime.theme.FlorisImeTheme
 import dev.patrickgold.florisboard.ime.theme.FlorisImeUi
+import dev.patrickgold.florisboard.lib.android.isOrientationPortrait
 import dev.patrickgold.florisboard.lib.snygg.ui.snyggBackground
 import dev.patrickgold.florisboard.lib.snygg.ui.snyggBorder
 import dev.patrickgold.florisboard.lib.snygg.ui.snyggShadow
@@ -52,6 +54,7 @@ fun QuickActionButton(
     modifier: Modifier = Modifier,
 ) {
     val context = LocalContext.current
+    val configuration = LocalConfiguration.current
 
     var isPressed by remember { mutableStateOf(false) }
     val actionStyle = FlorisImeTheme.style.get(FlorisImeUi.SmartbarQuickAction, isPressed = isPressed)
@@ -91,7 +94,7 @@ fun QuickActionButton(
                 }
                 if (iconResId != null) {
                     Icon(
-                        modifier = Modifier.padding(2.dp),
+                        modifier = Modifier.padding(if (configuration.isOrientationPortrait()) 2.dp else 0.dp),
                         painter = painterResource(id = iconResId),
                         contentDescription = null,
                         tint = actionStyle.foreground.solidColor(default = FlorisImeTheme.fallbackContentColor()),
