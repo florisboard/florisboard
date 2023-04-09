00c84ecfec9ea15fe345bacb5c4248f0731e25b7
Basti <>
2023-04-09 21:11:30 +0200

    Add x86_64 abi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,8 @@ android {
         }
 
         ndk {
-            abiFilters += listOf("armeabi-v7a", "arm64-v8a")
+            abiFilters += listOf("x86_64")
+            //abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86_64")
         }
 
         sourceSets {
