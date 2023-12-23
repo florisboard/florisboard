7e1bce2cead97b8076f549b78b0cba2a1004c811
Patrick Goldinger <patrick@patrickgold.dev>
2023-12-23 03:11:50 +0100

    Fix IME enabled/selected checker failing on API 34+ (#2344)

--- app/src/main/kotlin/dev/patrickgold/florisboard/lib/util/InputMethodUtils.kt
+++ app/src/main/kotlin/dev/patrickgold/florisboard/lib/util/InputMethodUtils.kt
@@ -21,45 +21,82 @@ import android.content.Context
 import android.content.Intent
 import android.provider.Settings
 import android.view.inputmethod.InputMethodManager
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
+import dev.patrickgold.florisboard.BuildConfig
 import dev.patrickgold.florisboard.lib.android.AndroidSettings
+import dev.patrickgold.florisboard.lib.android.AndroidVersion
 import dev.patrickgold.florisboard.lib.android.systemServiceOrNull
 import dev.patrickgold.florisboard.lib.devtools.flogDebug
+import kotlinx.coroutines.delay
 
 private const val DELIMITER = ':'
 private const val IME_SERVICE_CLASS_NAME = "dev.patrickgold.florisboard.FlorisImeService"
+private const val TIMED_QUERY_DELAY = 500L
 
 object InputMethodUtils {
     fun isFlorisboardEnabled(context: Context): Boolean {
-        val enabledImeList = AndroidSettings.Secure.getString(context, Settings.Secure.ENABLED_INPUT_METHODS)
-        return enabledImeList != null && parseIsFlorisboardEnabled(context, enabledImeList)
+        return if (AndroidVersion.ATLEAST_API34_U) {
+            context.systemServiceOrNull(InputMethodManager::class)
+                ?.enabledInputMethodList
+                ?.any { it.packageName == BuildConfig.APPLICATION_ID } ?: false
+        } else {
+            val enabledImeList = AndroidSettings.Secure.getString(
+                context, Settings.Secure.ENABLED_INPUT_METHODS
+            )
+            enabledImeList != null && parseIsFlorisboardEnabled(context, enabledImeList)
+        }
     }
 
     fun isFlorisboardSelected(context: Context): Boolean {
-        val selectedIme = AndroidSettings.Secure.getString(context, Settings.Secure.DEFAULT_INPUT_METHOD)
-        return selectedIme != null && parseIsFlorisboardSelected(context, selectedIme)
+        return if (AndroidVersion.ATLEAST_API34_U) {
+            context.systemServiceOrNull(InputMethodManager::class)
+                ?.currentInputMethodInfo
+                ?.packageName == BuildConfig.APPLICATION_ID
+        } else {
+            val selectedIme = AndroidSettings.Secure.getString(
+                context, Settings.Secure.DEFAULT_INPUT_METHOD
+            )
+            selectedIme != null && parseIsFlorisboardSelected(context, selectedIme)
+        }
     }
 
     @Composable
     fun observeIsFlorisboardEnabled(
         context: Context = LocalContext.current.applicationContext,
         foregroundOnly: Boolean = false,
-    ) = AndroidSettings.Secure.observeAsState(
-        key = Settings.Secure.ENABLED_INPUT_METHODS,
-        foregroundOnly = foregroundOnly,
-        transform = { parseIsFlorisboardEnabled(context, it.toString()) },
-    )
+    ): State<Boolean> {
+        return if (AndroidVersion.ATLEAST_API34_U) {
+            timedObserveIsFlorisBoardEnabled()
+        } else {
+            AndroidSettings.Secure.observeAsState(
+                key = Settings.Secure.ENABLED_INPUT_METHODS,
+                foregroundOnly = foregroundOnly,
+                transform = { parseIsFlorisboardEnabled(context, it.toString()) },
+            )
+        }
+    }
 
     @Composable
     fun observeIsFlorisboardSelected(
         context: Context = LocalContext.current.applicationContext,
         foregroundOnly: Boolean = false,
-    ) = AndroidSettings.Secure.observeAsState(
-        key = Settings.Secure.DEFAULT_INPUT_METHOD,
-        foregroundOnly = foregroundOnly,
-        transform = { parseIsFlorisboardSelected(context, it.toString()) },
-    )
+    ): State<Boolean> {
+        return if (AndroidVersion.ATLEAST_API34_U) {
+            timedObserveIsFlorisBoardSelected()
+        } else {
+            AndroidSettings.Secure.observeAsState(
+                key = Settings.Secure.DEFAULT_INPUT_METHOD,
+                foregroundOnly = foregroundOnly,
+                transform = { parseIsFlorisboardSelected(context, it.toString()) },
+            )
+        }
+    }
 
     fun parseIsFlorisboardEnabled(context: Context, activeImeIds: String): Boolean {
         flogDebug { activeImeIds }
@@ -89,5 +126,33 @@ object InputMethodUtils {
         } else {
             false
         }
+    }
+
+    @RequiresApi(api = 34)
+    @Composable
+    private fun timedObserveIsFlorisBoardEnabled(): State<Boolean> {
+        val state = remember { mutableStateOf(false) }
+        val context = LocalContext.current
+        LaunchedEffect(Unit) {
+            while (true) {
+                state.value = isFlorisboardEnabled(context)
+                delay(TIMED_QUERY_DELAY)
+            }
+        }
+        return state
+    }
+
+    @RequiresApi(api = 34)
+    @Composable
+    private fun timedObserveIsFlorisBoardSelected(): State<Boolean> {
+        val state = remember { mutableStateOf(false) }
+        val context = LocalContext.current
+        LaunchedEffect(Unit) {
+            while (true) {
+                state.value = isFlorisboardSelected(context)
+                delay(TIMED_QUERY_DELAY)
+            }
+        }
+        return state
     }
 }
