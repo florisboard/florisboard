f0d6e728857508c61ad18d5ceddadb79d64d2bb1
lm41 <lm41@lm41.xyz>
2024-06-20 19:07:14 +0200

    remove deprecated swipe-able API and switch to M3 ModalBottomSheet

--- app/src/main/kotlin/dev/patrickgold/florisboard/FlorisCopyToClipboardActivity.kt
+++ app/src/main/kotlin/dev/patrickgold/florisboard/FlorisCopyToClipboardActivity.kt
@@ -10,49 +10,32 @@ import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.BottomSheetValue
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.SwipeableDefaults
-import androidx.compose.material.rememberSwipeableState
-import androidx.compose.material.swipeable
-import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
-import dev.patrickgold.florisboard.ime.sheet.BottomSheetHostUi
-import dev.patrickgold.florisboard.ime.theme.FlorisImeTheme
-import dev.patrickgold.florisboard.ime.theme.FlorisImeUi
+import dev.patrickgold.florisboard.app.apptheme.FlorisAppTheme
+import dev.patrickgold.florisboard.app.florisPreferenceModel
 import dev.patrickgold.florisboard.lib.android.AndroidClipboardManager
 import dev.patrickgold.florisboard.lib.android.stringRes
 import dev.patrickgold.florisboard.lib.android.systemService
 import dev.patrickgold.florisboard.lib.compose.ProvideLocalizedResources
 import dev.patrickgold.florisboard.lib.compose.stringRes
-import dev.patrickgold.florisboard.lib.snygg.ui.snyggBackground
-import dev.patrickgold.florisboard.lib.snygg.ui.snyggClip
-import dev.patrickgold.florisboard.lib.snygg.ui.solidColor
-import dev.patrickgold.florisboard.lib.snygg.ui.spSize
-import kotlin.math.roundToInt
+import dev.patrickgold.jetpref.datastore.model.observeAsState
 
 class FlorisCopyToClipboardActivity : ComponentActivity() {
     private var error: CopyToClipboardError? = null
@@ -79,12 +62,13 @@ class FlorisCopyToClipboardActivity : ComponentActivity() {
         super.onPause()
     }
 
-    @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val systemClipboardManager = this.systemService(AndroidClipboardManager::class)
         val type = intent.type
         val action = intent.action
+
+        val prefs by florisPreferenceModel()
 
         if (Intent.ACTION_SEND != action || type == null) {
             error = CopyToClipboardError.UNKNOWN_ERROR
@@ -116,77 +100,26 @@ class FlorisCopyToClipboardActivity : ComponentActivity() {
 
         setContent {
             ProvideLocalizedResources(this, forceLayoutDirection = LayoutDirection.Ltr) {
-                FlorisImeTheme {
-                    BottomSheetHostUi(isShowing = true, onHide = { finish() }) {
-                        val panelStyle = FlorisImeTheme.style.get(FlorisImeUi.SmartbarActionsEditor)
-                        val headerStyle = FlorisImeTheme.style.get(FlorisImeUi.SmartbarActionsEditorHeader)
-                        val subheaderStyle = FlorisImeTheme.style.get(FlorisImeUi.SmartbarActionsEditorSubheader)
-                        val context = LocalContext.current
-                        Swipable {
-                            Column(
+                val theme by prefs.advanced.settingsTheme.observeAsState()
+                val isMaterialYouAware by prefs.advanced.useMaterialYou.observeAsState()
+                FlorisAppTheme(theme, isMaterialYouAware) {
+                    BottomSheet {
+                        Row {
+                            Text(
+                                text = error?.showError()
+                                    ?: bitmap?.let { stringRes(id = R.string.send_to_clipboard__description__copied_image_to_clipboard) }
+                                    ?: stringRes(R.string.send_to_clipboard__unknown_error),
+                                textAlign = TextAlign.Center,
+                                modifier = Modifier.weight(1f),
+                            )
+                        }
+                        bitmap?.let {
+                            Image(
                                 modifier = Modifier
-                                    .snyggBackground(
-                                        context,
-                                        panelStyle,
-                                        fallbackColor = FlorisImeTheme.fallbackSurfaceColor()
-                                    )
-                                    .snyggClip(panelStyle)
-                            ) {
-                                Row(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .snyggBackground(context, headerStyle),
-                                    verticalAlignment = Alignment.CenterVertically,
-                                ) {
-                                    Spacer(modifier = Modifier.weight(1F))
-                                    BottomSheetDefaults.DragHandle(
-                                        color = headerStyle.foreground.solidColor(
-                                            context,
-                                            default = FlorisImeTheme.fallbackContentColor()
-                                        ),
-                                    )
-                                    Spacer(modifier = Modifier.weight(1F))
-                                }
-                                Row(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .snyggBackground(context, headerStyle),
-                                    verticalAlignment = Alignment.CenterVertically,
-                                ) {
-                                    Text(
-                                        text = error?.showError()
-                                            ?: bitmap?.let { stringRes(id = R.string.send_to_clipboard__description__copied_image_to_clipboard) }
-                                            ?: stringRes(R.string.send_to_clipboard__unknown_error),
-                                        color = headerStyle.foreground.solidColor(
-                                            context,
-                                            default = FlorisImeTheme.fallbackContentColor()
-                                        ),
-                                        fontSize = headerStyle.fontSize.spSize(),
-                                        textAlign = TextAlign.Center,
-                                        modifier = Modifier.weight(1f),
-                                    )
-                                    Spacer(Modifier.height(48.dp))
-                                }
-                                bitmap?.let {
-                                    Image(
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .padding(8.dp),
-                                        bitmap = bitmap!!.asImageBitmap(),
-                                        contentDescription = null
-                                    )
-                                }
-                                Button(
-                                    onClick = { finish() },
-                                    modifier = Modifier.align(alignment = Alignment.End),
-                                    colors = ButtonDefaults.textButtonColors(
-                                        //containerColor = buttonContainer.background.solidColor(context = context),
-                                        contentColor = subheaderStyle.foreground.solidColor(context = context),
-                                    )
-                                ) {
-                                    Text(text = stringRes(id = R.string.action__ok))
-                                }
-                            }
+                                    .padding(start = 64.dp, end = 64.dp, top = 32.dp, bottom = 8.dp),
+                                bitmap = bitmap!!.asImageBitmap(),
+                                contentDescription = null
+                            )
                         }
                     }
                 }
@@ -194,51 +127,28 @@ class FlorisCopyToClipboardActivity : ComponentActivity() {
         }
     }
 
-    @OptIn(ExperimentalMaterialApi::class)
+
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
-    internal fun Swipable(
-        content: @Composable () -> Unit
+    internal fun BottomSheet(
+        content: @Composable ColumnScope.() -> Unit,
     ) {
-        val swipeableState = rememberSwipeableState(
-            initialValue = BottomSheetValue.Expanded,
-            confirmStateChange = {
-                if (it == BottomSheetValue.Collapsed) {
-                    finish()
-                }
-                true
-            }
-        )
-        BoxWithConstraints {
-            val constraintsScope = this
-            val maxHeight = with(LocalDensity.current) {
-                constraintsScope.maxHeight.toPx()
-            }
-            Box(
-                modifier = Modifier
-                    .swipeable(
-                        state = swipeableState,
-                        orientation = Orientation.Vertical,
-                        anchors = mapOf(
-                            0f to BottomSheetValue.Expanded,
-                            maxHeight to BottomSheetValue.Collapsed,
-                        ),
-                        resistance = SwipeableDefaults.resistanceConfig(
-                            anchors = setOf(0f, maxHeight),
-                            factorAtMin = 0F
-                        )
-                    )
-                    .offset {
-                        IntOffset(
-                            x = 0,
-                            y = swipeableState.offset.value.roundToInt()
-                        )
-                    }
-            ) {
+        ModalBottomSheet(
+            modifier = Modifier.navigationBarsPadding(),
+            onDismissRequest = { finish() }
+        ) {
+            Column {
                 content()
+                Button(
+                    modifier = Modifier.align(Alignment.End).padding(16.dp),
+                    onClick = { finish() },
+                    colors = ButtonDefaults.textButtonColors(
+                        //containerColor = buttonContainer.background.solidColor(context = context),
+                    )
+                ) {
+                    Text(text = stringRes(id = R.string.action__ok))
+                }
             }
         }
     }
 }
-
-
-
