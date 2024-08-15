f8e41710328fe4b303f24d81641fad0f3aff7144
lm41 <lm41@lm41.xyz>
2024-08-16 00:22:06 +0200

    Fix transparent navigation bar color on older api levels

--- app/src/main/kotlin/dev/patrickgold/florisboard/app/apptheme/Theme.kt
+++ app/src/main/kotlin/dev/patrickgold/florisboard/app/apptheme/Theme.kt
@@ -18,17 +18,13 @@ package dev.patrickgold.florisboard.app.apptheme
 
 import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
