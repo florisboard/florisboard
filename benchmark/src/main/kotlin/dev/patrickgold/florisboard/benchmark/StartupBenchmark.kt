123b3a6d395e4baa6e6842e792cca72a1540864a
Patrick Goldinger <patrick@patrickgold.dev>
2025-01-13 00:05:38 +0100

    Update copyright year and name in all source files

--- benchmark/src/main/kotlin/dev/patrickgold/florisboard/benchmark/StartupBenchmark.kt
+++ benchmark/src/main/kotlin/dev/patrickgold/florisboard/benchmark/StartupBenchmark.kt
@@ -1,6 +1,5 @@
 /*
- * Copyright 2022 The Android Open Source Project
- * Copyright (C) 2022 Patrick Goldinger
+ * Copyright (C) 2022-2025 The FlorisBoard Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
