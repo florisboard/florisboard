d994da8c97e9267bc3aa0de6278688a4caa905e7
Patrick Goldinger <patrick@patrickgold.dev>
2025-07-15 22:30:31 +0200

    Rework containerization and add track logic

---
+++ utils/repr-build/assemble.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+readonly TRACK_STABLE="stable"
+readonly TRACK_PREVIEW="preview"
+readonly TRACK_DEBUG="debug"
+
+TRACK="$1"
+OUTPUT_DIR="out"
+
+if [ "$TRACK" = "$TRACK_STABLE" ]; then
+  GRADLE_TASK_LC="release"
+  GRADLE_TASK_TC="Release"
+  AAB_ENABLED=1
+elif [ "$TRACK" = "$TRACK_PREVIEW" ]; then
+  GRADLE_TASK_LC="beta"
+  GRADLE_TASK_TC="Beta"
+  AAB_ENABLED=1
+elif [ "$TRACK" = "$TRACK_DEBUG" ]; then
+  GRADLE_TASK_LC="debug"
+  GRADLE_TASK_TC="Debug"
+  AAB_ENABLED=0
+else
+  echo "fatal: specified unknown track '$TRACK', aborting"
+  exit 1
+fi
+
+build() {
+  local TYPE="$1"
+  BUILD_LOG_PATH="build.log"
+  if [ "$TYPE" = "apk" ]; then
+    TASK="assemble$GRADLE_TASK_TC"
+    BUILD_DIR="app/build/outputs/apk/$GRADLE_TASK_LC"
+  elif [ "$TYPE" = "aab" ]; then
+    TASK="bundle$GRADLE_TASK_TC"
+    BUILD_DIR="app/build/outputs/bundle/$GRADLE_TASK_LC"
+  else
+    echo "fatal: unknown type '$TYPE', aborting..."
+  fi
+  echo -n "build: $TASK ... "
+  if ./gradlew "$TASK" > "$BUILD_LOG_PATH" 2>&1; then
+    echo "done"
+  else
+    echo "failed, aborting..."
+    exit 1
+  fi
+  BUILD_METADATA_JSON="$BUILD_DIR/output-metadata.json"
+  BUILD_FILE_NAME="$(jq -r ".elements[].outputFile" "$BUILD_METADATA_JSON")"
+  BUILD_VERSION_NAME="$(jq -r ".elements[].versionName" "$BUILD_METADATA_JSON")"
+  BUILD_FILE_PATH="$BUILD_DIR/$BUILD_FILE_NAME"
+  if [ "$TRACK" = "$TRACK_DEBUG" ]; then
+    OUTPUT_FILE_PATH="$OUTPUT_DIR/florisboard-$BUILD_VERSION_NAME.$TYPE"
+  else
+    OUTPUT_FILE_PATH="$OUTPUT_DIR/florisboard-$BUILD_VERSION_NAME-$TRACK.$TYPE"
+  fi
+  OUTPUT_LOG_PATH="$OUTPUT_FILE_PATH.log"
+  echo "generated $TYPE: $BUILD_FILE_PATH"
+  if [ ! -f "$BUILD_FILE_PATH" ]; then
+    echo "fatal: $TYPE not generated, aborting..."
+    exit 1
+  fi
+  cp "$BUILD_FILE_PATH" "$OUTPUT_FILE_PATH" || exit 1
+  mv "$BUILD_LOG_PATH" "$OUTPUT_LOG_PATH" || exit 1
+}
+
+mkdir "$OUTPUT_DIR"
+build apk
+if [ "$AAB_ENABLED" -eq 1 ]; then
+  build aab
+fi
--- utils/repr-build/run.sh
+++ utils/repr-build/run.sh
@@ -28,23 +28,18 @@ read_property ndk                   NDK_VERSION
 read_property rustup                RUSTUP_VERSION
 read_property rustToolchain         RUST_TOOLCHAIN_VERSION
 
-#if docker images | grep "$IMAGE_NAME" | grep -q "$IMAGE_TAG"; then
-#  echo "Docker image $IMAGE_NAME:$IMAGE_TAG exists, using it"
-#else
-#  echo "Docker image $IMAGE_NAME:$IMAGE_TAG does not exist, building it"
-  docker build -t "$IMAGE_NAME:$IMAGE_TAG" -f "utils/repr-build/Dockerfile" . \
-    --build-arg "PROJECT_COMPILE_SDK=$PROJECT_COMPILE_SDK" \
-    --build-arg "CMDLINE_TOOLS_VERSION=$CMDLINE_TOOLS_VERSION" \
-    --build-arg "CMDLINE_TOOLS_CHECKSUM=$CMDLINE_TOOLS_CHECKSUM" \
-    --build-arg "BUILD_TOOLS_VERSION=$BUILD_TOOLS_VERSION" \
-    --build-arg "CMAKE_VERSION=$CMAKE_VERSION" \
-    --build-arg "JDK_VERSION=$JDK_VERSION" \
-    --build-arg "NDK_VERSION=$NDK_VERSION" \
-    --build-arg "RUSTUP_VERSION=$RUSTUP_VERSION" \
-    --build-arg "RUST_TOOLCHAIN_VERSION=$RUST_TOOLCHAIN_VERSION" \
-    --build-arg "REPO_MOUNT=$REPO_MOUNT" \
-    || exit 1
-#fi
+docker build -t "$IMAGE_NAME:$IMAGE_TAG" -f "utils/repr-build/Dockerfile" . \
+  --build-arg "PROJECT_COMPILE_SDK=$PROJECT_COMPILE_SDK" \
+  --build-arg "CMDLINE_TOOLS_VERSION=$CMDLINE_TOOLS_VERSION" \
+  --build-arg "CMDLINE_TOOLS_CHECKSUM=$CMDLINE_TOOLS_CHECKSUM" \
+  --build-arg "BUILD_TOOLS_VERSION=$BUILD_TOOLS_VERSION" \
+  --build-arg "CMAKE_VERSION=$CMAKE_VERSION" \
+  --build-arg "JDK_VERSION=$JDK_VERSION" \
+  --build-arg "NDK_VERSION=$NDK_VERSION" \
+  --build-arg "RUSTUP_VERSION=$RUSTUP_VERSION" \
+  --build-arg "RUST_TOOLCHAIN_VERSION=$RUST_TOOLCHAIN_VERSION" \
+  --build-arg "REPO_MOUNT=$REPO_MOUNT" \
+  || exit 1
 
 docker_run_it() {
   docker run --rm -it \
@@ -53,31 +48,37 @@ docker_run_it() {
 }
 
 docker_run() {
+  docker run --rm \
+    -w "$REPO_MOUNT" \
+    "$IMAGE_NAME:$IMAGE_TAG" "$@"
+}
+
+docker_run_assemble() {
   local container
   container="fl-build-$(uuidgen)" || {
     echo "fatal: uuidgen not available, aborting"
     exit 1
   }
+  local track
+  track="$1"
   docker run --name "$container" \
     -w "$REPO_MOUNT" \
-    "$IMAGE_NAME:$IMAGE_TAG" "$@"
+    "$IMAGE_NAME:$IMAGE_TAG" ./assemble.sh "$track" \
+    && {
+      docker cp "$container:$REPO_MOUNT/out" "."
+    }
   docker rm "$container"
 }
 
 action="$1"
+shift
 
 case "$action" in
-  "interactive")
+  "interactive"|"it")
     docker_run_it /bin/bash
     ;;
-  "assemble:stable")
-    docker_run ./gradlew assembleRelease --no-daemon
-    ;;
-  "assemble:preview")
-    docker_run ./gradlew assembleBeta --no-daemon
-    ;;
-  "assemble:debug")
-    docker_run ./gradlew assembleDebug --no-daemon
+  "assemble")
+    docker_run_assemble "$@"
     ;;
   "clean")
     docker_run ./gradlew clean --no-daemon
